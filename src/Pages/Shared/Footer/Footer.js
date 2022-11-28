13b1e0ccdbaba9d309b2dd4461c1f5049a7b5e46
nazmu957 <nazmus15-1999@diu.edu.bd>
2022-11-28 22:42:02 +0600

    footer updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,17 +6,16 @@ const Footer = () => {
            <footer className="footer p-10 bg-red-100 text-base-content">
   <div>
     <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
+    <a className="link link-hover">Sell Chair</a> 
+    <a className="link link-hover">Sell table</a> 
+    <a className="link link-hover">Sell Kitchen Furniture</a> 
+ 
   </div> 
   <div>
     <span className="footer-title">Furniture Mania</span> 
     <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
+    <a className="link link-hover">Contact</a>  
+    <a className="link link-hover">Discount Policy</a>
   </div> 
   <div>
     <span className="footer-title">Legal</span> 
