8126a6b74d84d27b653dd6984a8a043dc1310e12
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-07-25 07:21:37 +0600

    remove extra option from navbar

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = () => {
          {menuItems}
         </ul>
       </div>
-        <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+        {/* <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -72,7 +72,7 @@ const Navbar = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </label>
+          </label> */}
     </div>
   )
 }
