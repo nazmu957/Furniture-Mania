f722b848b65a2bf400260c69efc2faeb9727292a
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-07-25 07:41:07 +0600

    resize the single review for different device

--- src/Pages/Home/SingleReview/SingleReview.js
+++ src/Pages/Home/SingleReview/SingleReview.js
@@ -9,9 +9,9 @@ const SingleReview = () => {
     <div className="hero ">
       <img src={banner4} alt=""/>
   <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Are You Looking for Used Furniture?</h1>
+  <div className="hero-content text-center">
+    <div className="text-white">
+      <h1 className="mb-5  text-[1.5rem] lg:text-[3.5rem]">Are You Looking for Used Furniture?</h1>
       <p className="mb-5">
 We've delivered quality furniture to more than half a million North American homes and businesses. Article is staffed by engineers, journalists, painters, weight lifters, slack-liners, mountain bikers, chefs, reiki masters, and more.</p>
       <Link to="/blog"><button className="btn btn-primary">Learn more</button></Link>
